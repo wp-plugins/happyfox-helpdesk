819d40cea7c61bd61e9adcd5843596f00e4c7756
Agent.Logic_ <Agent.Logic_@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-30 11:39:43 +0000

    Deleted wrong files
    git-svn-id: https://plugins.svn.wordpress.org/happyfox-helpdesk/tags/1.0.0@905532 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 1.0.0/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//if uninstall not called from WordPress exit
-	if (!defined('WP_UNINSTALL_PLUGIN')) {
-		exit();
-	}
-
-	$happyfox_settings = 'happyfox_settings';
-	delete_option($happyfox_settings);
-?>
